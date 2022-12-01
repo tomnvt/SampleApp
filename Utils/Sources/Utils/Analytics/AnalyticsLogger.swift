f315ba07beaeef699b2d78e8507af9a8e2d3c830
Tomáš Novotný <tomas.novotny@synetech.cz>
2022-12-01 17:44:21 +0100

    Added logger

--- Utils/Sources/Utils/Analytics/AnalyticsLogger.swift
+++ Utils/Sources/Utils/Analytics/AnalyticsLogger.swift
@@ -2,6 +2,7 @@ import FirebaseAnalytics
 
 public class AnalyticsLogger {
     public static func logEvent(_ event: AnalyticEvent) {
+        Logger.logAnalytics("\(event.eventName): parameters: \(String(describing: event.parameters))")
         Analytics.logEvent(event.eventName, parameters: event.parameters)
     }
 }
